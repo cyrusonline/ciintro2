24d473c7d78726c350788517e61a66255cf0f7e7
C.K. Chan <chanchikin@ied.edu.hk>
2015-08-13 22:39:06 +0800

    try again

---
+++ application/views/view_about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title><?php echo $title?></title>
+</head>
+<body>
+<div id="container">
+<h1>Welcome to about</h1>
+
+<a href="home">Home</a>
+<a href="about">About</a>
+</div>
+
+
+</body>
+</html>
---
+++ application/views/view_home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title><?php echo $title?></title>
+</head>
+<body>
+<h3>Welcome to my world</h3>
+<h3><?php echo $var1?></h3>
+<p>The addition of <?php echo $var1?> and <?php echo $var2?> is <?php echo $addTotal?></p>
+<a href="home">Home</a>
+<a href="about">About</a>
+
+
+</body>
+</html>
