56e3a4fffae00be55840a898f79b9745493b6f23
C.K. Chan <chanchikin@ied.edu.hk>
2015-08-14 17:04:52 +0800

    start delete

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -43,4 +43,57 @@ class Site extends CI_Controller {
 		$this->load->view("view_db",$data);
 		
 	}
+	
+	function insertValues(){
+		$this->load->model("get_db");
+		$newRow = array(
+			array(
+					
+					"name"=>"Sue"
+					
+			),
+				array(
+							
+						"name"=>"Dylan"
+			
+				)
+				
+				
+				
+			
+		);
+		
+		$this->get_db->insert2($newRow);
+		echo "added";
+		
+	}
+	
+	function updateValues(){
+		$this->load->model("get_db");
+		
+		$newRow = array(
+				array(
+					"id"=>"3","name"=>"cyrus"
+						
+				),
+				array(
+						"id"=>"5","name"=>"WONG"
+				
+				)
+		);
+		$this->get_db->update2($newRow);
+		
+		echo "updated";
+		
+		
+	}
+	
+
+	function deleteValues(){
+		$this->load->model("get_db");
+	
+		
+	
+	
+	}
 }
--- application/models/get_db.php
+++ application/models/get_db.php
@@ -5,4 +5,24 @@
 			$query=$this->db->query("SELECT * FROM test");
 			return $query->result();
 		}
+		
+		function insert1($data){
+						
+			$this->db->insert("test",$data);
+			
+			
+		}
+		
+		function insert2($data){
+			$this->db->insert_batch("test", $data);
+			
+		}
+		
+		function update($data){
+			$this->db->update("test",$data,"id=2");
+				
+		}
+		function update2($data){
+			$this->db->update_batch("test",$data,"id");
+		}
 	}
